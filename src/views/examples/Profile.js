3ca060bc4b9c2295f3b3bc738dcedc0238a4358a
Saja Malkawi <sajamalkawi95@gmail.com>
2021-09-09 23:25:26 +0300

    build

--- src/views/examples/Profile.js
+++ src/views/examples/Profile.js
@@ -23,7 +23,7 @@ import { Button, Card, Container, Row, Col } from "reactstrap";
 // core components
 import DemoNavbar from "components/Navbars/DemoNavbar.js";
 import SimpleFooter from "components/Footers/SimpleFooter.js";
-import profile from "assets/img/theme/team-4-800x800.jpg";
+import profile from "assets/img/personal/3.jpeg";
 
 class Profile extends React.Component {
   componentDidMount() {
@@ -108,47 +108,43 @@ class Profile extends React.Component {
                     <Col className="order-lg-1" lg="4">
                       <div className="card-profile-stats d-flex justify-content-center">
                         <div>
-                          <span className="heading">22</span>
-                          <span className="description">Friends</span>
+                          <span className="heading">2</span>
+                          <span className="description"> Friends </span>
                         </div>
                         <div>
-                          <span className="heading">10</span>
-                          <span className="description">Photos</span>
+                          <span className="heading">7</span>
+                          <span className="description"> Photos </span>
                         </div>
                         <div>
-                          <span className="heading">89</span>
-                          <span className="description">Comments</span>
+                          <span className="heading"> 5 </span>
+                          <span className="description">Projects</span>
                         </div>
                       </div>
                     </Col>
                   </Row>
                   <div className="text-center mt-5">
                     <h3>
-                      Jessica Jones{" "}
-                      <span className="font-weight-light">, 27</span>
+                      Saja Malkawi{" "}
+                      <span className="font-weight-light">, 25 </span>
                     </h3>
                     <div className="h6 font-weight-300">
                       <i className="ni location_pin mr-2" />
-                      Bucharest, Romania
+                     Irbid , Jordan
                     </div>
                     <div className="h6 mt-4">
                       <i className="ni business_briefcase-24 mr-2" />
-                      Solution Manager - Creative Tim Officer
+                      Software Developer Intern
                     </div>
                     <div>
                       <i className="ni education_hat mr-2" />
-                      University of Computer Science
+                       Mutah University 
                     </div>
                   </div>
                   <div className="mt-5 py-5 border-top text-center">
                     <Row className="justify-content-center">
                       <Col lg="9">
                         <p>
-                          An artist of considerable range, Ryan — the name taken
-                          by Melbourne-raised, Brooklyn-based Nick Murphy —
-                          writes, performs and records all of his own music,
-                          giving it a warm, intimate feel with a solid groove
-                          structure. An artist of considerable range.
+                        Software developer with a background in Computer Science. Passionate about programming, interested in building websites and mobile apps using (Java, HTML, CSS, javascript, and Bootstrap). In October 2020 I started three months of training in Atypon company in (problem-solving, Java, Data Structure, and Algorithm). In June 2021 I started training with ASAC to be a full-stack web developer for six months
                         </p>
                         <a href="#pablo" onClick={e => e.preventDefault()}>
                           Show more
